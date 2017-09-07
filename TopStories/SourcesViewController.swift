7379b9d3caae59c7ad00bc8258569c017e1311e9
Student <student@amajeed.local>
2017-09-07 12:37:35 -0500

    Finish Initial setup

--- TopStories/SourcesViewController.swift
+++ TopStories/SourcesViewController.swift
@@ -14,16 +14,51 @@ class SourcesViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        super.viewDidLoad():
         self.title = "News Sources"
         let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
         
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["status"] == "ok" {
+                    parse(json: json)
+                    return
+                }
+            }
+        }
+        loadError()
     }
+    func parse(json: JSON)
+    {
+        for result in json["sources"].arrayValue
+        {
+            let id = result["id"].stringValue
+            let name = result["name"].stringValue
+            let description = result["description"].stringValue
+            let source = ["id": id, "name": name, "description": description]
+            sources.append(source)
+        }
+        tableView.reloadData()
+    }
+    func loadError()
+    {
+        let alert = UIAlertController(title: "Loading Error", message: "There was a problem loading the news feed", preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
 
-
+        present(alert, animated: true, completion: nil)
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return sources.count//number of news organization
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let source = sources[indexPath.row]
+        cell.textLabel?.text = source["name"]
+        cell.detailTextLabel?.text = source["description"]
+        return cell
+    }
 }
 
